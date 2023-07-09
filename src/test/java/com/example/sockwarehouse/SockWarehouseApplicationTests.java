7daf823b2b8f1082b25cc5ed4be13cd3a6683947
Anastasia <nastay.dr2002@gmail.com>
2023-07-09 20:08:14 +0400

    Initial commit

---
+++ src/test/java/com/example/sockwarehouse/SockWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sockwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
